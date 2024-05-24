53cbe054fbf654f92f2ef699443badf2708ce3d5
Gbangbolaoluwagbemiga <oluwagbemigagbangbola@gmail.com>
2024-05-24 23:15:02 +0100

    Today init

--- src/component/Header.jsx
+++ src/component/Header.jsx
@@ -1,22 +1,25 @@
 import { UseMonad } from "../context/UseMonad";
 
 function Header() {
-  const { setMonadMode, monadMode} = UseMonad();
+  const { setMonadMode, monadMode } = UseMonad();
 
-
-function handleMode(){
-  monadMode?alert("Monad mode deactivated"):alert("Monad mode deactivated")
-  setMonadMode(prev=>!prev)
-}
+  function handleMode() {
+    monadMode ? alert("Monad mode activated") : alert("Monad mode deactivated");
+    setMonadMode((prev) => !prev);
+  }
 
   return (
-    <div className={`flex justify-between p-4 ${monadMode?"bg-[#5246A3]":"bg-white"}`}>
+    <div
+      className={`flex justify-between p-4 ${
+        monadMode ? "bg-[#5246A3]" : "bg-white"
+      }`}
+    >
       <div className="flex">
         <h2 className=" text-xl font-bold md:text-3xl">MONAD</h2>
         <img
           src="https://static.chainbroker.io/mediafiles/projects/monad/monad.jpeg"
           alt="Loader Image"
-          className=" spinner cursor-pointer ms-2 h-8 w-8 rounded-full"
+          className=" spinner ms-2 h-8 w-8 cursor-pointer rounded-full"
           onClick={handleMode}
         />
       </div>
