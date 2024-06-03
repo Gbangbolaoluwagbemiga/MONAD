170e48352bcc9bac41f22b8994c9022de47a526d
Gbangbolaoluwagbemiga <oluwagbemigagbangbola@gmail.com>
2024-06-03 22:26:54 +0100

    Done

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import Arts from "./Evilord/Arts";
 import Evolution from "./Evilord/EvilHead";
+import Footer from "./Evilord/Footer";
 import Timer from "./Evilord/Timer";
 import Header from "./component/Header";
 import Intro from "./component/Intro";
@@ -33,6 +34,7 @@ function App() {
           <Timer />
           <Evolution />
           <Arts />
+          <Footer />
         </div>
       )}
     </div>
--- src/Evilord/Footer.jsx
+++ src/Evilord/Footer.jsx
@@ -1,5 +1,22 @@
 function Footer() {
-  return <div></div>;
+  return (
+    <div className="text-center text-xl font-bold text-white">
+      <h1 className="mb-5 ">
+        For further information you can check out the socials below
+      </h1>
+
+      <div className="mb-5 mt-2 flex justify-center gap-4">
+        <a href="https://twitter.com/gangsterallstar">
+          <i className="fa-brands fa-x-twitter me-8 text-2xl text-white md:text-3xl"></i>
+        </a>
+        <a href="https://discord.gg/gangsterallstar">
+          <i className="fa-brands fa-discord me-8 text-2xl text-white md:text-3xl"></i>
+        </a>
+      </div>
+
+      <p>All right reserved, A dedication by Cryptic-Dev.</p>
+    </div>
+  );
 }
 
 export default Footer;
